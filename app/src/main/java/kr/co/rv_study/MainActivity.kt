6a6d2a8b87dcd41ae1b5afb342ba03ef2ee8c18c
JISOO2206 <900522z@gmail.com>
2023-01-16 23:39:01 +0900

    액티비티 라이프 사이클 확인하는 로직 생성

--- app/src/main/java/kr/co/rv_study/MainActivity.kt
+++ app/src/main/java/kr/co/rv_study/MainActivity.kt
@@ -2,10 +2,42 @@ package kr.co.rv_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("main", "[create]")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Log.d("main", "[start]")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d("main", "[resume]")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("main", "[pause]")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("main", "[stop]")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d("main", "[Destroy]")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("main", "[reStart]")
+        super.onRestart()
     }
 }
