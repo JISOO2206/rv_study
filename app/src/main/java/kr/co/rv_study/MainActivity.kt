8daeb68a353262e540ecdb0de5826c155b80464e
JISOO2206 <900522z@gmail.com>
2023-01-18 23:47:01 +0900

    로그 제거, Layout 높이 설정 수정

--- app/src/main/java/kr/co/rv_study/MainActivity.kt
+++ app/src/main/java/kr/co/rv_study/MainActivity.kt
@@ -2,13 +2,11 @@ package kr.co.rv_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("main", "[create]")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
